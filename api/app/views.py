8dd4d396adfaa3e507a9df30a4a9a256ca2d54ee
Fredy <freorozcoloa@gmail.com>
2022-01-31 22:58:58 -0500

    All is updated

--- api/app/views.py
+++ api/app/views.py
@@ -3,6 +3,6 @@ from .utilis import get_model, transform_to_dataframe
 
 model = get_model()
 def get_prediction(request:PredictionRequest)->float:
-    data_to_predcit = transform_to_dataframe(request)
-    prediction = model.predict(data_to_predcit)[0]
+    data_to_predict = transform_to_dataframe(request)
+    prediction = model.predict(data_to_predict)[0]
     return max(0, prediction)
