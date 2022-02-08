7819af56632f3e5151d603ffe0dc9b595f559375
Fredy <freorozcoloa@gmail.com>
2022-02-08 00:14:04 -0500

    Agg library dotenv

--- utilities/setter.py
+++ utilities/setter.py
@@ -1,10 +1,9 @@
 
 import os
 from base64 import b64decode
-from dotenv import load_dotenv
+
 def main():
-    load_dotenv()
-    key = os.getenv('SERVICE_ACCOUNT_KEY')
+    key = os.environ.get('SERVICE_ACCOUNT_KEY')
     with open('path.json', "w") as json_file:
         json_file.write(b64decode(key).decode())
     print(os.path.realpath("path.json"))
